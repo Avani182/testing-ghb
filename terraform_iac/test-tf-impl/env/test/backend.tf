9e11180f99ab4728d9e499f246694002bbe1b2c2
Avani182 <91719362+Avani182@users.noreply.github.com>
2023-12-30 18:44:25 -0500

    Update backend.tf

--- terraform_iac/test-tf-impl/env/test/backend.tf
+++ terraform_iac/test-tf-impl/env/test/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "tf-remote-backend-bucket-esrcgs9er7"
     key            = "terraform.tfstate"
-    region         = "ca-central-1"
+    region         = "us-east-1"
     dynamodb_table = "tf-remote-backend-dynamodb-table"
   }
 }
